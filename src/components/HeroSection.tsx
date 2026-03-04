4df357ed2aaf5c1cfc35c7ee63c67fe5344b170d
teamvisionloop <teamvisionloop@gmail.com>
2026-03-04 16:33:18 +0200

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import heroBg from "@/assets/hero-bg.jpeg";
+import heroBg from "@/assets/hero-i.webp";
 
 interface HeroSectionProps {
   onStartCustomizing: () => void;
